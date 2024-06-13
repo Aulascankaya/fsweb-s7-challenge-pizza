8dce22c65d642b3a5bc8ed2f711ada632d99968f
A. Ulaş Çankaya <aulascankaya@gmail.com>
2024-06-13 13:18:09 +0300

    Success page finished

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Success() {
   return (
-    <div>Success</div>
-  )
+    <div className="orderSuccess">
+      <img src="./Assets/mile1-assets/tek-yemek.png" alt="logo" />
+      <p>TEBRİKLER!</p>
+      <p>SİPARİŞİNİZ ALINDI!</p>
+    </div>
+  );
 }
 
-export default Success
+export default Success;
