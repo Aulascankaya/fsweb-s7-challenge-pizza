2397de6bb1fa926a3ea473e2f7609bfa6bdef7d6
A. Ulaş Çankaya <aulascankaya@gmail.com>
2024-06-24 21:13:26 +0300

    Footer Created

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ import {
 import Home from "./components/Home";
 import OrderForm from "./components/OrderForm";
 import Success from "./components/Success";
-import "./components/Component.css"
-import "./App.css"
+import "./components/Component.css";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-
+      <Header />
       <Switch>
         <Route exact path="/">
           <Home />
@@ -25,9 +27,10 @@ function App() {
           <Success />
         </Route>
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
-//commit test 
+//commit test
 let test;
 export default App;
