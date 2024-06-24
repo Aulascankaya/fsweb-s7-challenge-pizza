48f378a4b2289d5d23e390217e399f102db010ee
A. Ulaş Çankaya <aulascankaya@gmail.com>
2024-06-24 19:05:14 +0300

    Home Page Updated Fonts added images added css updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,18 +4,70 @@ import TopIcons from "./TopIcons";
 import Banner from "./Banner";
 
 function Home() {
+  let history = useHistory();
 
-  let history = useHistory()
-
-    function handleClick(){
-        history.push("/orderForm")
-    }
-
+  function handleClick() {
+    history.push("/orderForm");
+  }
 
   return (
     <div className="home-page">
       <Banner onClick={handleClick} />
-      <TopIcons/>
+      <TopIcons />
+      <div className="first-section">
+        <div className="images-section">
+          <div className="ozel-lezzetus">
+            <div>
+              <h2>
+                <span>Özel</span>
+                <span>Lezzetus</span>
+              </h2>
+              <p>Position Absolute Acı Burger</p>
+              <button
+                className="siparis-buton"
+                type="button"
+                onClick={handleClick}
+              >
+                SİPARİŞ VER
+              </button>
+            </div>
+          </div>
+          <div className="hackathlon-npm-container">
+            <div className="hackathlon">
+              <h4>
+                <span>Hackathlon </span>
+                <span>Burger Menü</span>
+              </h4>
+              <button
+                className="siparis-buton"
+                type="button"
+                onClick={handleClick}
+              >
+                SİPARİŞ VER
+              </button>
+            </div>
+            <div className="npm">
+              <h4>
+                <span style={{ color: "#ce2829" }}>Çooooook</span>
+                <span>hızlı</span>
+                <span>npm gibi kurye</span>
+              </h4>
+              <button
+                className="siparis-buton"
+                type="button"
+                onClick={handleClick}
+              >
+                SİPARİŞ VER{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="context">
+          <p className="first-text">en çok paketlenen menüler</p>
+          <p className="second-text">Acıktıran Kodlara Doyuran Lezzetler</p>
+        </div>
+      </div>
     </div>
   );
 }
